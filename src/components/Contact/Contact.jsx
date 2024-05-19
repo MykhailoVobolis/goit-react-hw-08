1bcf85df84c56aa239660da64a5eae385e50ebb4
MykhailoVobolis <mvobolis@gmail.com>
2024-05-19 13:51:12 +0300

    style pack 1.1

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,20 +12,23 @@ export default function Contact({ contacts: { id, name, number } }) {
   const handleDelete = () => dispatch(deleteContact(id));
 
   return (
-    <div className={css.container}>
-      <div className={css.containerItem}>
-        <p>
-          <FaUser className={css.icon} />
-          {name}
-        </p>
-        <p>
-          <FaPhone className={css.icon} />
-          {number}
-        </p>
+    <div className={css.mainContainer}>
+      <div className={css.border}></div>
+      <div className={css.container}>
+        <div className={css.containerItem}>
+          <p>
+            <FaUser className={css.icon} />
+            {name}
+          </p>
+          <p>
+            <FaPhone className={css.icon} />
+            {number}
+          </p>
+        </div>
+        <button className={css.btnDel} onClick={handleDelete}>
+          Delete
+        </button>
       </div>
-      <button className={css.btnDel} onClick={handleDelete}>
-        Delete
-      </button>
     </div>
   );
 }
