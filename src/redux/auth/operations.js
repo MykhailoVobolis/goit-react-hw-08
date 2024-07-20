720cd84b52a849d379a3250eb8c47c479faf0146
MykhailoVobolis <mvobolis@gmail.com>
2024-07-21 00:24:05 +0300

    changed the server baseURL

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://connections-api.herokuapp.com";
+// axios.defaults.baseURL = "https://connections-api.herokuapp.com";
+axios.defaults.baseURL = "https://connections-api.goit.global";
 
 const setAuthHeader = (token) => {
   axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
